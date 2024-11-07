f967cb6ff28861ae2457a3f9d7b0908a49eed6c0
Aryan20018 <160220600+Aryan20018@users.noreply.github.com>
2024-11-07 22:42:01 +0530

    jslearning

---
+++ loops.js
@@ -0,0 +1,24 @@
+// const greeting = "Hello world!"
+// for (const greet of greeting) {
+//     if (greet === "" ) {
+//         break;
+//     }
+//     console.log(`Each char is ${greet}`);
+    
+// }
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const num= myNums.filter( (num) => num> 4 )
+//  console.log(num);
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         console.log(num)
+//     }
+// } )
+
+// node
+console.log( myNums[myNums.length -1]);
+console.log(1);

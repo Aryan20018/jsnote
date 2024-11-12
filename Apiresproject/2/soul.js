0ebc20684ecfefd51a1bff15a36fae58f6197dcc
Aryan20018 <160220600+Aryan20018@users.noreply.github.com>
2024-11-12 18:53:20 +0530

    removeadded

--- Apiresproject/2/soul.js
+++ Apiresproject/2/soul.js
@@ -13,10 +13,12 @@
 
 
 const startButton = document.querySelector('#start')
+const hideButton = document.querySelector('#stop')
 const image = document.querySelector('#img')
 const names = document.querySelector('#name')
 const followers = document.querySelector('#followers')
 const follow = document.querySelector('#follow')
+const avtar = document.querySelector('.image')
 
 const repopublic = document.querySelector('#public_repos')
 
@@ -25,8 +27,34 @@ startButton.addEventListener('click', function (e) {
     console.log('clicked');
     dataCall()
 })
+hideButton.addEventListener('click', function (c) {
+    console.log("clicked");
+    //    remove dataCall();
+    names.innerHTML = `<h2></h2>`
+    followers.innerHTML = `<h2></h2>`
+    follow.innerHTML = `<h2></h2>`
+    repopublic.innerHTML = `<h2></h2>`
+    image.src = ``;
+    //   image.card.visibility="hidden";
+    // console.log(c.target.parentNode);
+    // function hider(){}
+    // removeIt. 
+    // image.src.remove()
+    // avtar.card.visibility="hidden";
+    //    hide();  
+})
+// function hide() { 
+//     if (document.getElementById) { 
+//     document.getElementById('.image').style.visibility = 'hidden'; 
+//     } 
+// }
 
-
+// function hider(){
+//     target.image
+//     console.log(c.target.parentNode);
+//     // const myImage = document.getElementById("myP");
+//     // myImage.style.visibility = (myImage.style.visibility === "hidden") ? '' : "hidden"; 
+// }
 function dataCall() {
     const requestUrl = 'https://api.github.com/users/hiteshchoudhary'
     const xhr = new XMLHttpRequest();
@@ -35,35 +63,19 @@ function dataCall() {
         console.log(xhr.readyState);
         if (xhr.readyState === 4) {
             const data = JSON.parse(this.responseText)
-            console.log(data);
-            console.log(data.login);
+            // console.log(data);
+            // console.log(data.login);
             // names.innerHTML= `<h2>${data.login}</h2>`;
             names.innerText = data.name === null ? data.login : data.name;
-            // data.followers
-            // data.following
-            // data.avatar_url
-            followers.innerHTML= `${ data.followers}`;
-            follow.innerHTML= `${ data.following}`;
+            followers.innerHTML = `${data.followers}`;
+            follow.innerHTML = `${data.following}`;
             image.src = `${data.avatar_url}`;
-           
-            repopublic.innerHTML= `${data.public_repos}`;
-        }   
+            repopublic.innerHTML = `${data.public_repos}`;
+
+        }
+
     }
     xhr.send();
+
 }
 
-// function nameshow(names){
-//         names.innerHTML= `<h2>${names}</h2>`;
-// }
-
-
-
-
-
-
-
-
-
-
-
-// document.querySelector("button.follow").addEventListener('click',showCardId);
